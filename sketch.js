115e89df98194e512f0cd2818360680aacbcf4b3
goingobsolete <goingobsolete@gmail.com>
2024-06-11 11:44:40 +0200

    initial commit

---
+++ sketch.js
@@ -0,0 +1,32 @@
+let brushSizeX = 30;
+let brushSizeY = 30
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+  background(255);
+
+}
+
+function draw() {
+  // Set the brush color to black
+  fill(random(100),random(0),random(255), random(100));
+  noStroke();
+  brushSizeX = random(100);
+  brushSizeY = random(100);
+  // brushSizeX = 30;
+  // brushSizeY = 30;
+  
+  
+  // Draw a circle at the mouse position to reveal the text
+  if (mouseIsPressed) {
+    ellipse(mouseX, mouseY, brushSizeX, brushSizeY);
+    
+  push();
+  textSize(width/6);
+  textFont('Helvetica');
+  textAlign(CENTER, CENTER);
+  fill(255);
+  text("A D A E L", width / 2, height / 2);
+  pop();
+
+  }
+}
